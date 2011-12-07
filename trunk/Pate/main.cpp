4a30dfb3b7aeeb2bb8dde6bf38b370dd192b0cac
patemotter <patemotter@f078c7f8-09aa-5818-66fe-f92f6643b314>
2011-12-07 17:43:46 +0000

    kernels.cl - Created kernel for matrix inverse main.cpp - Added two buffers and commands to run matrix inverse kernel The matrix is hardcoded currently, but will be changed to be dynamic.

--- trunk/Pate/main.cpp
+++ trunk/Pate/main.cpp
@@ -92,6 +92,12 @@ int main(){
         ((float*)X)[i] = (float)matrix[i];
     }
 
+	//Create the Identity matrix for testing
+	cl_float *Min = (cl_float *)malloc(3*3*sizeof(cl_float));
+	Min[0] = 1; Min[1] = 3; Min[2] = 1;
+	Min[3] = 1; Min[4] = 1; Min[5] = 2;
+	Min[6] = 2; Min[7] = 3; Min[8] = 4;
+
     // Load the kernel source code into the array source_str
     FILE *fp;
     char *source_str;
@@ -121,11 +127,13 @@ int main(){
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
  
     // Create memory buffers on the device for each vector 
-    cl_mem matrix_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,numCons*numVars * sizeof(cl_float), NULL, &ret);
-    cl_mem output_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numCons*numVars * sizeof(cl_float), NULL, &ret);
+    cl_mem matrix_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE,numCons*numVars * sizeof(cl_float), NULL, &ret);
+	cl_mem Min_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, 3*3*sizeof(cl_float), NULL, &ret);
+	cl_mem Mout_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, 3*3*sizeof(cl_float), NULL, &ret);
  
     // Copy the lists X and Y to their respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, matrix_mem_obj, CL_TRUE, 0, numCons*numVars * sizeof(cl_float), X, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(command_queue, Min_mem_obj, CL_TRUE, 0, 3*3*sizeof(cl_float), Min, 0, NULL, NULL);
  
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
@@ -134,24 +142,33 @@ int main(){
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
  
     // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "invert", &ret);
+    cl_kernel kernel = clCreateKernel(program, "inverse", &ret);
  
     // Set the arguments of the kernel
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&matrix_mem_obj);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&output_mem_obj);
-	cl_int matrixSize = numCons*numVars;
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_int), (void *)&matrixSize);
- 
+    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&Min_mem_obj);
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&Mout_mem_obj);
+	cl_int actualSize = 3;
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_int), (void*)&actualSize);
+	
+	/*
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&matrix_mem_obj);
+	cl_int matrixSize = numCons;
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_int), (void *)&matrixSize);
+	*/
+
+
     // Execute the OpenCL kernel on the list
     ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
+
+	cl_float meh[9] = {1.1};
  
     // Read the memory buffer C on the device to the local variable C
-    cl_float *C = (cl_float*)malloc(sizeof(cl_float)*numCons*numVars);
-    ret = clEnqueueReadBuffer(command_queue, output_mem_obj, CL_TRUE, 0,numCons*numVars * sizeof(cl_float), C, 0, NULL, NULL);
+   // ret = clEnqueueReadBuffer(command_queue, matrix_mem_obj, CL_TRUE, 0,numCons*numVars * sizeof(cl_float), X, 0, NULL, NULL);
+	ret = clEnqueueReadBuffer(command_queue, Mout_mem_obj, CL_TRUE, 0, 3*3*sizeof(cl_float), meh, 0, NULL, NULL);
  
     // Display the result to the screen
-    for(int i = 0; i < numCons*numVars; i++)
-		cout << C[i] << " ";
+    for(int i = 0; i < 9; i++)
+		cout << meh[i] << " ";
 	cout << endl;
  
     // Clean up
@@ -160,12 +177,11 @@ int main(){
     ret = clReleaseKernel(kernel);
     ret = clReleaseProgram(program);
     ret = clReleaseMemObject(matrix_mem_obj);
-    ret = clReleaseMemObject(output_mem_obj);
     ret = clReleaseCommandQueue(command_queue);
     ret = clReleaseContext(context);
     free(X);
-    free(C);
   
+  cout << "Program is now finished...\n";
   cin.get();
   return 0;
 }
